5c678ad89ddfc6d07eb000e2720ba2bdcc4dbccb
anthemnandani <nandani@antheminfotech.com>
2025-09-01 15:11:06 +0530

    commited

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -59,7 +59,7 @@ const AuthPage = () => {
                             transition={{ duration: 0.6 }}
                             className="w-1/2 flex flex-col justify-center px-8 py-12 bg-white rounded-tr-[150px] rounded-br-[150px] text-black z-10"
                         >
-                            <div className="max-w-sm mx-auto">
+                            <div className="min-w-sm mx-auto">
                                 <h3 className="text-3xl font-bold text-gray-900 mb-6 leading-tight">
                                     Create Your HelpDesk Account
                                 </h3>
@@ -189,7 +189,7 @@ const AuthPage = () => {
                             transition={{ duration: 0.6 }}
                             className="w-1/2 flex flex-col justify-center px-8 py-12 bg-white rounded-tl-[150px] rounded-bl-[150px] text-black order-2 z-10"
                         >
-                            <div className="max-w-sm mx-auto">
+                            <div className="min-w-sm mx-auto">
                                 <h3 className="text-3xl font-bold text-gray-900 mb-6 leading-tight">
                                     Login to HelpDesk
                                 </h3>
@@ -289,13 +289,19 @@ const AuthPage = () => {
 
                         <div className="flex flex-col items-center text-center max-w-lg">
                             <div className="mb-8 relative">
-                                <div className="w-80 h-80 bg-white/10 backdrop-blur-sm rounded-full flex items-center justify-center">
+                                     {!isLogin ? (
                                     <img
-                                        src="https://res.cloudinary.com/dmyq2ymj9/image/upload/v1756709421/vecteezy_woman-in-headphones-with-laptop-for-customer-support_50894418-removebg-preview_nnmdau.png"
-                                        alt="Help Desk Illustration"
-                                        className="w-72 h-auto object-contain"
+                                        src="https://res.cloudinary.com/dmyq2ymj9/image/upload/v1756718304/output-onlinepngtools_nptfvm.png"
+                                        alt="Signup Logo"
+                                        className="w-[500px] h-auto object-contain"
                                     />
-                                </div>
+                                ) : (
+                                    <img
+                                        src="https://res.cloudinary.com/dmyq2ymj9/image/upload/v1756717798/Help-Desk-PNG-Picture_ftycge.png"
+                                        alt="Login Logo"
+                                        className="w-[500px] h-auto object-contain"
+                                    />
+                                )}
                                 <div className="absolute -top-4 -right-4 w-16 h-16 bg-purple-300/30 rounded-full animate-pulse"></div>
                                 <div className="absolute -bottom-6 -left-6 w-12 h-12 bg-white/20 rounded-full animate-pulse delay-700"></div>
                                 <div className="absolute top-1/2 -left-8 w-8 h-8 bg-purple-300/40 rounded-full animate-pulse delay-1000"></div>
